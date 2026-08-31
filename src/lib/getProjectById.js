28406509a087eea08e895c6e1cfe9ea010923ad8
sabujAlom <sabujalom18@gmail.com>
2026-08-31 18:59:18 +0600

    bug issue solution

--- src/lib/getProjectById.js
+++ src/lib/getProjectById.js
@@ -1,7 +1,7 @@
 // app/lib/getProjectById.js
 export default async function getProjectById(id) {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}projects/${id}`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/projects/${id}`, {
       cache: 'no-store', // or 'force-cache' / revalidate, depending on your needs
     })
     if (!res.ok) {
